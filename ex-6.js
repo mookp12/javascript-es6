eebdb1097769900a48b64ca4d07345e1bb648528
mookp12 <kraiviwat.p@gmail.com>
2025-07-30 13:51:20 +0700

    feat:ex1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,10 @@
 let height = undefined;
 let result;
 
+function newFunction (height) {
+    let result = height ?? 'Height is not defined';
+    return result
+}
+
+result = newFunction(height)
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
